e5d22b405acd78addc52549f610518764ae28cb8
Transell <transell@o2.pl>
2016-01-27 16:21:58 +0100

    Update Calculation.java

--- Calculation.java
+++ Calculation.java
@@ -8,10 +8,13 @@ import com.GuessNumber.View.ViewText;
 public class Calculation
 {
 
+	public final int MAX_ATTEMPTS = 101;
+	public final int MAX_TURNS_NUMBER = 6;
+
 	public void runProgram()
 	{
 		Random random = new Random();
-		int variate = random.nextInt(101);
+		int variate = random.nextInt(MAX_ATTEMPTS);
 		ViewText.explanation();
 		Scanner scanner = new Scanner(System.in);
 		int play = scanner.nextInt();
@@ -20,24 +23,25 @@ public class Calculation
 
 	public void testVariantsOfGuessResult(int variate, Scanner scanner, int play)
 	{
-		for (int i = 0; i < 6;)
+		for (int i = 0; i < MAX_TURNS_NUMBER; i++)
 		{
 			if (play == variate)
 			{
 				ViewText.win();
 				break;
-			} else if (play > variate)
+			} 
+			else if (play > variate)
 			{
 				ViewText.numberIsLower();
-				play = scanner.nextInt();
-				i++;
-			} else if (play < variate)
+			} 
+			else if (play < variate)
 			{
 				ViewText.numberIsHigher();
-				play = scanner.nextInt();
-				i++;
 			}
-			if (i == 6)
+
+			play = scanner.nextInt();
+
+			if (i == MAX_TURNS_NUMBER)
 			{
 				ViewText.outOfAttempts();
 			}
