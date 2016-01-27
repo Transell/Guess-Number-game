5aeaf645b18a33d594375bbd67c40b05dabbc36c
Transell <transell@o2.pl>
2016-01-27 15:12:17 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.GuessNumber.Main;
+
+import com.GuessNumber.Calculation.Calculation;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Calculation calculation = new Calculation();
+		calculation.runProgram();
+	}
+
+}
